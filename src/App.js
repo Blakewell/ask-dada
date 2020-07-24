a6cb2d93119eb68890dda8652f3bc0974373d67a
Blake Blackwell <blacblak@amazon.com>
2020-07-24 16:32:58 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Hello World!
         </p>
         <a
           className="App-link"
