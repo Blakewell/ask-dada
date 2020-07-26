ed4251d26a36eb9e8e697bf5ea46b946de250b5c
Blake Blackwell <blacblak@amazon.com>
2020-07-25 20:43:19 -0500

    adding routing

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
 /* src/App.js */
-import React, { useEffect, useState } from 'react'
-import Amplify, { API, graphqlOperation } from 'aws-amplify'
-import { listTodos } from './graphql/queries'
+import React from 'react'
+import Amplify from 'aws-amplify'
 import { withAuthenticator } from '@aws-amplify/ui-react'
 
 /* bootstrap stuff */
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import awsExports from "./aws-exports";
-import { Navbar, Container, Row, Col} from 'react-bootstrap'
+import { Navbar, Nav} from 'react-bootstrap'
 import ToDoForm from './components/ToDoForm'
 import ToDoTable from './components/ToDoTable'
+import Switch from 'react-bootstrap/esm/Switch';
+import {Link, Route} from 'react-router-dom'
 
 Amplify.configure(awsExports);
 
 const App = () => {
   return (
     <div>
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand>Ask Dada</Navbar.Brand>
-      </Navbar>
-      <Container>
-        <Row>
-          <Col>
-            <ToDoForm />
-         </Col>
-        </Row>
-        <Row></Row>
-        <Row>
-          <Col>
-          <ToDoTable />
-          </Col>
-        </Row>
-        </Container>
+      <div>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand>Ask Dada</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link as={Link} to="/Add">Add</Nav.Link>
+            <Nav.Link as={Link} to="/List">List</Nav.Link>
+          </Nav>
+        </Navbar>
+      </div>
+      <div>
+        <Switch>
+          <Route exact path='/Add' component={ToDoForm} />
+          <Route exact path='/List' component={ToDoTable} />
+        </Switch>
+      </div>
     </div>
   )
 }
